5ff3f4c7b244d734f631b6d92cca974570384507
dinoosati1978 <dinoosati1978@gmail.com>
2023-03-11 13:30:20 +0530

    initial commit

--- lambda/main.tf
+++ lambda/main.tf
@@ -1,3 +1,29 @@
+provider "aws" {
+  region = "us-east-2"
+
+  default_tags {
+    tags = {
+      type = "lambda-api-gateway"
+    }
+  }
+
+}
+
+resource "random_pet" "lambda_bucket_name" {
+  prefix = "learn-terraform-functions"
+  length = 4
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = random_pet.lambda_bucket_name.id
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+  acl    = "private"
+}
+
+
 data "archive_file" "lambda_hello_world" {
   type = "zip"
 
